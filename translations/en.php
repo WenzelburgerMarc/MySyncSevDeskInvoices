42570af6c8c8cbdc29298279ad54a9451b2334ed
Marc Wenzelburger <marc.wenzelburger@pangoon.de>
2023-11-16 14:02:29 +0100

    Add - Translations

---
+++ translations/en.php
@@ -0,0 +1,24 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_1472ba9973548a4383f888715c13073e'] = ' Sync SevDesk Invoices';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_cef1901b22e51fb5961b29651f9bec51'] = 'This Module Synchronizes PrestaShop Invoices With SevDesk';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_e7b83a44d4b211b2625a36733f1c7670'] = 'Are You Sure That You Want To Uninstall This Module? No More Invoices Will Get Synchronized With SevDesk!';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_f4f70727dc34561dfde1a3c529b6205c'] = 'Settings';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_1f37743cdcdef44b3d80e8c166c6c3ce'] = 'API URL';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_2e451706fda770ebf122b21bfb11e70e'] = 'Enter your SevDesk API URL here. For Example: https://my.sevdesk.de/api/v1/';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_5161b4f9ce9a8b7d966e8bf3c049f6f3'] = 'API Token';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_d7a7d1b63c3b93782437cdd44f8a1ec6'] = 'Enter your SevDesk API Token here.';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_242d897a763c476bf5c05c5e79ee3ea7'] = 'Days Until Delivery';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_5c613025331539f6b9582fc88ff19a97'] = 'Order Date + Days Until Delivery = Delivery Date in Invoice';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_6f39234a2effa71be57ffa243303bade'] = 'Male Title';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_f773f20d3fecfbd86e31946fd812d9fb'] = 'Title for male (e.g., Mr.)';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_49725c73597a477cf57114d99c95e7b2'] = 'Female Title';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_35dc65d4d82a9969d761290b4cfd04f4'] = 'Title for female (e.g., Mrs. or Ms.)';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_52a825370d56d10b463023fec254d4fa'] = 'Neutral Title';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_cea45288ac36f05d713317b92fa4e1af'] = 'Title for neutral (leave blank if not applicable)';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_c9cc8cce247e49bae79f15173ce97354'] = 'Save';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_c888438d14855d7d96a2724ee9c306bd'] = 'Settings updated';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>configure_1472ba9973548a4383f888715c13073e'] = 'Sync SevDesk Invoices';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>configure_5a0f3c752223f0cfe6e1e34589858afa'] = 'Set your sevDesk API token here. This setting will synchronize all upcoming invoices. Please enter old invoices manually.';
