50c191e67fad5a27529e4c9abdffebcd2c7ffb2c
Marc Wenzelburger <marc.wenzelburger@pangoon.de>
2023-11-20 14:46:41 +0100

    Add - Reload Overview Button

--- src/controller/AdminSyncController.php
+++ src/controller/AdminSyncController.php
@@ -11,6 +11,32 @@ use Symfony\Component\HttpFoundation\Response;
 class AdminSyncController extends FrameworkBundleAdminController
 {
 
+    public function getLogDataAction(){
+        $logEntities = $this->getLogData();
+        $logs = [];
+        foreach ($logEntities as $entity) {
+            $logs[] = [
+                'id' => $entity->getId(),
+                'log' => $entity->getLog(),
+            ];
+        }
+        return new Response(json_encode($logs), 200, ['Content-Type' => 'application/json']);
+    }
+
+
+    public function getInvoiceDataAction(){
+        $invoiceData = $this->getInvoiceData();
+        $invoices = [];
+        foreach ($invoiceData as $entity) {
+            $invoices[] = [
+                'id_sevdesk_invoice' => $entity->getId(),
+                'id_order' => $entity->getOrderId(),
+                'id_sevdesk' => $entity->getSevDeskId(),
+            ];
+        }
+        return new Response(json_encode($invoices), 200, ['Content-Type' => 'application/json']);
+    }
+
     public function getLogData(){
         return $this->getDoctrine()->getRepository(Log::class)->findAll();
     }
@@ -38,14 +64,6 @@ class AdminSyncController extends FrameworkBundleAdminController
                 'invoices' => $invoices
             ]
         );
-    }
-
-    public function getLogsData(){
-
-    }
-
-    public function getInvoicesData(){
-
     }
 
 }
