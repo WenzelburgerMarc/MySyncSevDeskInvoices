246383b009560891eaf91b459ec83b54adc7aa32
Marc Wenzelburger <marc.wenzelburger@pangoon.de>
2023-11-20 14:49:57 +0100

    Add - Translations

--- translations/de.php
+++ translations/de.php
@@ -6,10 +6,10 @@ $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_1472ba9973548
 $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_cef1901b22e51fb5961b29651f9bec51'] = 'Dieses Modul synchronisiert PrestaShop-Rechnungen mit SevDesk';
 $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_e7b83a44d4b211b2625a36733f1c7670'] = 'Sind Sie sicher, dass Sie dieses Modul deinstallieren möchten? Es werden keine Rechnungen mehr mit SevDesk synchronisiert!';
 $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_f4f70727dc34561dfde1a3c529b6205c'] = 'Einstellungen';
-$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_1f37743cdcdef44b3d80e8c166c6c3ce'] = 'API-URL';
-$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_2e451706fda770ebf122b21bfb11e70e'] = 'Geben Sie hier Ihre SevDesk-API-URL ein. Zum Beispiel: https://my.sevdesk.de/api/v1/';
 $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_5161b4f9ce9a8b7d966e8bf3c049f6f3'] = 'API-Token';
 $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_d7a7d1b63c3b93782437cdd44f8a1ec6'] = 'Geben Sie hier Ihren SevDesk-API-Token ein.';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_1f37743cdcdef44b3d80e8c166c6c3ce'] = 'API-URL';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_bde0939cc432975405c3b41d6165791c'] = 'Geben Sie hier Ihre SevDesk-API-URL ein. Zum Beispiel: https://my.sevdesk.de/api/v1/';
 $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_242d897a763c476bf5c05c5e79ee3ea7'] = 'Tage bis zur Lieferung';
 $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_5c613025331539f6b9582fc88ff19a97'] = 'Bestelldatum + Tage bis zur Lieferung = Lieferdatum in Rechnung';
 $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_6f39234a2effa71be57ffa243303bade'] = 'Männlicher Titel';
@@ -18,6 +18,8 @@ $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_49725c73597a4
 $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_35dc65d4d82a9969d761290b4cfd04f4'] = 'Titel für Frauen (z. B. Frau)';
 $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_52a825370d56d10b463023fec254d4fa'] = 'Neutraler/Diverser Titel';
 $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_cea45288ac36f05d713317b92fa4e1af'] = 'Titel für neutrale/diverse Kunden (kann leer gelassen werden)';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_a138c5d3c5876f156d27392d973a5ee8'] = 'Rabatt Text';
+$_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_87848ba460685a6ed162175fd24b00a9'] = 'Text für Rabatt (Zum Beispiel: Rabatt)';
 $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_c9cc8cce247e49bae79f15173ce97354'] = 'Speichern';
 $_MODULE['<{mysyncsevdeskinvoices}prestashop>mysyncsevdeskinvoices_c888438d14855d7d96a2724ee9c306bd'] = 'Einstellungen aktualisiert';
 $_MODULE['<{mysyncsevdeskinvoices}prestashop>configure_1472ba9973548a4383f888715c13073e'] = 'SevDesk-Rechnungen synchronisieren';
